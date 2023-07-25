f83cc6fd5ea905f57005bf26af7280422b89147b
Pi1at <grand.anatol@ya.ru>
2023-07-26 00:14:57 +0300

    infra

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "rust devShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              exa
+              fd
+              (rust-bin.stable."1.68.0".default.override
+                {
+                  extensions = ["rust-src"];
+                })
+            ];
+
+            shellHook = ''
+              alias ls=exa
+              alias find=fd
+            '';
+          };
+        }
+    );
+}
