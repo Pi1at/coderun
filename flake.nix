c3f090435c540ca4d57cd8621727cafc757aa161
Pi1at <Pi1at@users.noreply.github.com>
2024-08-07 15:21:26 +0300

    fix(flake): update flake to reflect changes in used rust version

--- flake.nix
+++ flake.nix
@@ -7,38 +7,39 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , rust-overlay
+    , flake-utils
+    , ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
-        overlays = [(import rust-overlay)];
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        rustToolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        nativeBuildInputs = with pkgs;
+          [
+            just
+            cargo-generate
+            cargo-llvm-cov
+            cargo-watch
+            cargo-semver-checks
+            rustToolchain
+            pkg-config
+            openssl
+          ];
+        buildInputs = with pkgs; [ ];
       in
-        with pkgs; {
-          devShells.default = mkShell {
-            buildInputs = [
-              openssl
-              pkg-config
-              exa
-              fd
-              (rust-bin.stable."1.68.0".default.override
-                {
-                  extensions = ["rust-src"];
-                })
-            ];
-
-            shellHook = ''
-              alias ls=exa
-              alias find=fd
-            '';
-          };
-        }
+      {
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs nativeBuildInputs;
+        };
+      }
     );
 }
