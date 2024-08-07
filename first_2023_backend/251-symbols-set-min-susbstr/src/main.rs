f62c055657b3b7028a99e4498ad12b44ac961f4a
Pi1at <Pi1at@users.noreply.github.com>
2024-08-07 15:22:31 +0300

    refactor(251): use workspace lints

--- first_2023_backend/251-symbols-set-min-susbstr/src/main.rs
+++ first_2023_backend/251-symbols-set-min-susbstr/src/main.rs
@@ -1,14 +1,14 @@
 use std::{
     collections::HashMap,
     io::{self, BufRead},
-    iter::FromIterator,
 };
 fn run_me(s: &str, c: &str) -> usize {
     if s.is_empty() || c.is_empty() {
         return 0;
     };
     let s_chars = s.chars().collect::<Vec<_>>();
-    let mut window_freq: HashMap<char, isize> = HashMap::from_iter(c.chars().map(|c| (c, 1)));
+    let mut window_freq: HashMap<char, isize> =
+        c.chars().map(|c| (c, 1)).collect::<HashMap<_, _>>();
     let mut min_len: Option<usize> = None;
 
     let (mut left, mut right, mut count): (usize, usize, usize) = (0, 0, window_freq.len());
@@ -16,16 +16,17 @@ fn run_me(s: &str, c: &str) -> usize {
     while right < s_chars.len() {
         let end_char = &s_chars[right];
         right += 1;
+
         if let Some(f) = window_freq.get_mut(end_char) {
             *f -= 1;
             if *f == 0 {
-                count -= 1
+                count -= 1;
             }
         } else {
             // well...this was unclear
             count = window_freq.len();
             left = right;
-            window_freq = HashMap::from_iter(c.chars().map(|c| (c, 1)));
+            window_freq = c.chars().map(|c| (c, 1)).collect::<HashMap<_, _>>();
         };
 
         if count > 0 {
@@ -41,16 +42,12 @@ fn run_me(s: &str, c: &str) -> usize {
             if let Some(f) = window_freq.get_mut(start_char) {
                 *f += 1;
                 if *f > 0 {
-                    count += 1
+                    count += 1;
                 }
             };
         }
         // update window
-
-        min_len = Some(match min_len {
-            Some(x) => x.min(right - left + 1),
-            None => right - left + 1,
-        })
+        min_len = Some(min_len.map_or(right - left + 1, |x| x.min(right - left + 1)));
     }
     min_len.unwrap_or_default()
 }
@@ -60,6 +57,6 @@ fn main() {
     let mut line_iter = stdin.lock().lines();
     let s = line_iter.next().unwrap().unwrap();
     let c = line_iter.next().unwrap().unwrap();
-
+    drop(line_iter);
     println!("{}", run_me(&s, &c));
 }
