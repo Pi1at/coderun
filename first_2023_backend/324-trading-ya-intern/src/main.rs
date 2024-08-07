0031ef069508e97562b77a888de46b6f96a616c4
Pi1at <Pi1at@users.noreply.github.com>
2024-08-07 15:22:41 +0300

    refactor(324): simplify

--- first_2023_backend/324-trading-ya-intern/src/main.rs
+++ first_2023_backend/324-trading-ya-intern/src/main.rs
@@ -1,42 +1,23 @@
-use std::io::{self, BufRead};
+use std::{
+    cmp::Reverse,
+    io::{self, BufRead},
+};
 
 fn main() {
     let stdin = io::stdin();
-    let mut line_iter = stdin.lock().lines();
-    let nm = line_iter
-        .next()
-        .unwrap()
-        .unwrap()
-        .split_whitespace()
-        .flat_map(|s| s.parse::<usize>())
-        .collect::<Vec<_>>();
+    let mut line_iter = stdin.lock().lines().map_while(Result::ok);
+    let n_or_m = line_iter.next().unwrap().split_whitespace().flat_map(str::parse).min().unwrap();
 
-    let mut sellers = line_iter
-        .next()
-        .unwrap()
-        .unwrap()
-        .split_whitespace()
-        .map(|x| x.parse::<isize>().unwrap())
-        .collect::<Vec<_>>();
+    let mut sellers =
+        line_iter.next().unwrap().split_whitespace().flat_map(str::parse).collect::<Vec<usize>>();
     sellers.sort_unstable();
 
-    let mut buyers = line_iter
-        .next()
-        .unwrap()
-        .unwrap()
-        .split_whitespace()
-        .map(|x| x.parse::<isize>().unwrap())
-        .collect::<Vec<_>>();
-    buyers.sort_unstable_by_key(|k| -k);
+    let mut buyers =
+        line_iter.next().unwrap().split_whitespace().flat_map(str::parse).collect::<Vec<usize>>();
+    drop(line_iter);
+    buyers.sort_unstable_by_key(|&k| Reverse(k));
 
-    let res: isize = sellers
-        .iter()
-        .zip(buyers.iter())
-        .take(nm[0].min(nm[1]))
-        .map_while(|(s, b)| match b - s {
-            x if x > 0 => Some(x),
-            _ => None,
-        })
-        .sum();
-    println!("{}", res);
+    let res: usize =
+        sellers.into_iter().zip(buyers).take(n_or_m).map_while(|(s, b)| b.checked_sub(s)).sum();
+    println!("{res}");
 }
