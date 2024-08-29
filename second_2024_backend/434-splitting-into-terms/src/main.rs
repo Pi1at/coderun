de1769ccb6fe8c418d43807e792e38bc6e9c0bb2
Pi1at <Pi1at@users.noreply.github.com>
2024-08-29 20:52:06 +0300

    docs(434): add doc comment to find_partition function

--- second_2024_backend/434-splitting-into-terms/src/main.rs
+++ second_2024_backend/434-splitting-into-terms/src/main.rs
@@ -1,6 +1,13 @@
 use std::io::BufRead;
 
-// n<=40 by definition, should be fine
+/// Generates all partitions of a positive integer `n` using numbers up to `max_num`.
+///
+/// # Arguments
+///
+/// * `n` - The integer to partition.
+/// * `max_num` - The maximum number allowed in the partition.
+/// * `current_partition` - A mutable vector to store the current partition.
+/// * `result` - A mutable vector to store all partitions.
 fn find_partitions(
     n: usize,
     max_num: usize,
@@ -24,6 +31,7 @@ fn main() {
     let n: usize = std::io::stdin().lock().lines().next().unwrap().unwrap().parse().unwrap();
     let mut result = Vec::new();
     let mut current_partition = Vec::new();
+    // n<=40 by definition, should be fine
     find_partitions(n, n, &mut current_partition, &mut result);
     for cur in result {
         let res = cur.iter().map(|&x| x.to_string()).collect::<Box<_>>().join(" + ");
