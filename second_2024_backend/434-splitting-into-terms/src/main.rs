ecd29bb7c45b85953ab5571f9b0bf2d8f5a0f1b5
Pi1at <Pi1at@users.noreply.github.com>
2025-02-27 19:42:58 +0300

    refactor: restructure partitioning logic and improve output handling
    there was a compile error after toolchain update -> implement a bit "better" version

--- second_2024_backend/434-splitting-into-terms/src/main.rs
+++ second_2024_backend/434-splitting-into-terms/src/main.rs
@@ -1,40 +1,62 @@
-use std::io::BufRead;
+use std::io::{self, BufRead};
 
-/// Generates all partitions of a positive integer `n` using numbers up to `max_num`.
-///
-/// # Arguments
-///
-/// * `n` - The integer to partition.
-/// * `max_num` - The maximum number allowed in the partition.
-/// * `current_partition` - A mutable vector to store the current partition.
-/// * `result` - A mutable vector to store all partitions.
-fn find_partitions(
-    n: usize,
-    max_num: usize,
-    current_partition: &mut Vec<usize>,
-    result: &mut Vec<Vec<usize>>,
-) {
-    if n == 0 {
-        result.push(current_partition.clone());
-        return;
+// poor man u8 to ascii num representation
+// just flexing a bit
+fn append_itoa(buf: &mut String, v: u8) {
+    if v >= 100 {
+        buf.push((v / 100 + b'0') as char);
+        buf.push(((v / 10) % 10 + b'0') as char);
+    } else if v >= 10 {
+        buf.push((v / 10 + b'0') as char);
     }
-    for i in 1..=max_num {
-        if i <= n {
-            current_partition.push(i);
-            find_partitions(n - i, i, current_partition, result);
-            current_partition.pop();
-        }
+    buf.push((v % 10 + b'0') as char);
+}
+
+fn join_into(p: &[u8], buffer: &mut String) {
+    let mut iter = p.iter();
+    if let Some(&v) = iter.next() {
+        append_itoa(buffer, v);
+    }
+    for &v in iter {
+        buffer.push_str(" + ");
+        append_itoa(buffer, v);
     }
 }
 
-fn main() {
-    let n: usize = std::io::stdin().lock().lines().next().unwrap().unwrap().parse().unwrap();
-    let mut result = Vec::new();
-    let mut current_partition = Vec::new();
-    // n<=40 by definition, should be fine
-    find_partitions(n, n, &mut current_partition, &mut result);
-    for cur in result {
-        let res = cur.iter().map(|&x| x.to_string()).collect::<Box<_>>().join(" + ");
-        println!("{res}");
+fn solve(n: u8) -> String {
+    fn find_partitions(num: u8, buffer: &mut String) {
+        fn find_partitions_inner(
+            n: u8,
+            max_num: u8,
+            current_partition: &mut Vec<u8>,
+            buffer: &mut String,
+        ) {
+            if n == 0 {
+                // Build the partition string directly
+                join_into(current_partition, buffer);
+                buffer.push('\n');
+                return;
+            }
+
+            for i in 1..=max_num.min(n) {
+                current_partition.push(i);
+                find_partitions_inner(n - i, i, current_partition, buffer);
+                current_partition.pop();
+            }
+        }
+        let mut current_partition = Vec::new();
+        find_partitions_inner(num, num, &mut current_partition, buffer);
     }
+
+    // just random guess
+    let cap = (7 * (n as usize).pow(4)) / 10;
+    let mut res = String::with_capacity(cap);
+    find_partitions(n, &mut res);
+    res
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let res = io::stdin().lock().lines().next().unwrap()?.parse().map(solve)?;
+    print!("{res}");
+    Ok(())
 }
