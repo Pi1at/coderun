bc1a5ddc8e01080fe1eafddab93c4dc28e8f67db
Pi1at <Pi1at@users.noreply.github.com>
2025-03-15 12:29:18 +0300

    refactor: optimize solve function using BinaryHeap for efficiency

--- second_2024_backend/427-microwave/src/main.rs
+++ second_2024_backend/427-microwave/src/main.rs
@@ -1,51 +1,60 @@
-use std::collections::BTreeSet;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::io::BufRead;
 
-fn solve(n: u64, [coefs @ .., m]: &[u64; 3]) -> u64 {
-    //let m = coefs.last().unwrap();
-    let mut dist = vec![-1_i64; *m as usize];
-    let mut q = BTreeSet::new();
-    dist[0] = 0;
-    q.insert(0);
-    let mut answer = 0;
+fn solve(n: usize, c: &[u32]) -> usize {
+    // n <= 10^18
+    const EMPTY: usize = 10_usize.pow(18);
+    match c {
+        [coefs @ .., m] => {
+            let m = *m as usize;
+            let mut dist = vec![EMPTY; m];
+            dist[0] = 0;
 
-    while let Some(v) = q.pop_first() {
-        if v >= n {
-            continue;
-        }
-        let d = (n - 1) - v;
-        answer += 1 + d / *m;
+            let mut queue = BinaryHeap::new();
+            queue.push(Reverse(0));
 
-        for &coef in &coefs[0..2] {
-            let next_v = v + coef;
-            let mut residue = next_v % *m;
-            // if residue < 0 {
-            //     residue += *m;
-            // }
-            let residue = residue as usize;
+            let mut answer = 0;
 
-            if dist[residue] == -1 || (next_v as i64) < dist[residue] {
-                if dist[residue] != -1 {
-                    q.remove(&(dist[residue] as u64));
+            // get minimum element
+            while let Some(Reverse(v)) = queue.pop() {
+                if queue.peek().is_some_and(|Reverse(next_v)| *next_v == v) {
+                    continue;
                 }
-                dist[residue] = next_v as i64;
-                q.insert(next_v);
+                let d = (n - 1) - v;
+                answer += 1 + d / m;
+
+                for &coef in coefs {
+                    let next_v = v + (coef as usize);
+                    if next_v >= n {
+                        continue;
+                    }
+                    let residue = next_v % m;
+                    if dist[residue] == EMPTY {
+                        dist[residue] = next_v;
+                        queue.push(Reverse(next_v));
+                    }
+                }
             }
-        }
+            answer
+        },
+        [] => 1, //cover 0 case
     }
-    answer
 }
 
 fn main() {
     let mut lines = std::io::stdin().lock().lines().map_while(Result::ok);
-    let n: u64 = lines.next().unwrap().parse().unwrap();
-    let mut coefs =
-        lines.next().unwrap().split_ascii_whitespace().flat_map(str::parse).collect::<Vec<u64>>();
+    let n = lines.next().unwrap().parse().unwrap();
+    let mut coefs = lines
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .flat_map(str::parse)
+        .filter(|&v| (v as usize) < n)
+        .collect::<Vec<u32>>();
     drop(lines);
     coefs.sort_unstable();
-
-    let answer = solve(n, &coefs[0..3]);
-    println!("{answer}");
+    println!("{}", solve(n, &coefs));
 }
 
 // region:    --- Tests
@@ -72,6 +81,17 @@ mod tests {
     fn test_20() {
         assert_eq!(5, solve(20, &[6, 14, 34]));
     }
+
+    #[test]
+    fn test_20_() {
+        assert_eq!(5, solve(20, &[6, 14]));
+    }
+
+    #[test]
+    fn test_5() {
+        assert_eq!(5, solve(5, &[1]));
+    }
+
     #[test]
     fn test_10() {
         assert_eq!(8, solve(10, &[3, 4, 5]));
